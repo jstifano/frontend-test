772d6fb7081be390b821d10e4dae83238f39df04
Javier Stifano <jstifano18@gmail.com>
2018-01-21 21:20:50 -0400

    Container de logica para detalle de los emails

---
+++ src/email_detail/containers/email_container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import EmailDetailLayout from '../components/email_detail'
+
+class EmailContainer extends Component {
+	render(){
+		return (
+			<EmailDetailLayout />
+		)
+	}
+
+}
+
+export default EmailContainer
