faaa36debf43132ae10981f76b50394d5c1fff54
Javier Stifano <jstifano18@gmail.com>
2018-01-21 21:21:21 -0400

    Cracion y configuracion inicial del store del Redux

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux' // Store para la data del App.
 
 // Componentes utilizados
 import MainContainer from '../main_container/containers/main_container'
+import data from '../mail-data.json'
 
-render(<MainContainer />, App);
+const initialState = data; // Guardo info inicial en el store
+
+// Creo el store de Redux
+const store = createStore(
+	() => {},
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
+render(<MainContainer data={data}/>, App);
+
